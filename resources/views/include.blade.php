4629670bb6e9bbd06bbffcbfe9f83b6c178acae5
aditbest5 <aditbest5@gmail.com>
2024-08-24 09:07:17 +0700

    tes

---
+++ resources/views/include.blade.php
@@ -0,0 +1,65 @@
+
+
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="description" content="#">
+<meta name="keywords" content="Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+<meta name="author" content="#">
+
+<!-- Required Fremwork -->
+<link rel="stylesheet" type="text/css" href="/template/files/bower_components/bootstrap/dist/css/bootstrap.min.css">
+<!-- feather Awesome -->
+<link rel="stylesheet" type="text/css" href="/template/font-awesome-6.2.0/css/all.css">
+<link rel="stylesheet" type="text/css" href="/template/files/assets/icon/feather/css/feather.css">
+<!-- Style.css -->
+<link rel="stylesheet" type="text/css" href="/template/files/assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="/template/files/assets/css/umum.css">
+<!-- Data Table Css -->
+<link rel="stylesheet" type="text/css" href="/template/files/bower_components/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
+
+
+<!-- SweatAlert Awesome -->
+<script type="text/javascript" src="/template/files/assets/js/sweetalert2.js"></script>
+<!-- Required Jquery -->
+<script type="text/javascript" src="/template/files/bower_components/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="/template/files/bower_components/popper.js/dist/umd/popper.min.js"></script>
+<script type="text/javascript" src="/template/files/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/template/files/assets/js/pcoded.min.js"></script>
+
+<script src="/template/files/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="/template/files/bower_components/data-table/js/dataTables.bootstrap4.min.js"></script>
+<script type="text/javascript" src="/template/files/assets/js/crypto-js.js"></script>
+<!-- Chart js -->
+<script type="text/javascript" src="/template/files/bower_components/chart.js/dist/Chart.js"></script>
+<!-- Custom js -->
+<script type="text/javascript" src="/template/files/assets/js/umum.js"></script>
+<script type="text/javascript" src="/template/files/assets/js/sidemenu.js"></script>
+<script type="text/javascript" src="/template/files/assets/js/script.js"></script>
+
+<style>
+    .bg-rki {
+        background: #2A3B6A !important;
+    }
+    .pcoded-navbar {
+        overflow: auto;
+        scrollbar-width: thin;
+        scrollbar-color: #56699d transparent;
+    }
+
+    /* Custom scrollbar styles */
+    .pcoded-navbar::-webkit-scrollbar {
+        width: 12px;
+    }
+
+    .pcoded-navbar::-webkit-scrollbar-track {
+        background: transparent;
+    }
+
+    .pcoded-navbar::-webkit-scrollbar-thumb {
+        background-color: #56699d;
+        border: 3px solid transparent;
+        background-clip: padding-box;
+    }
+</style>
